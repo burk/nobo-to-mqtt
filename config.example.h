7a931144cc9a68942d59b69f802e7f473fed1dfd
Bjørn Rustad <bjorn@rustad.me>
2024-02-10 18:25:23 +0100

    Default config

---
+++ config.example.h
@@ -0,0 +1,15 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Move to config.h and configure.
+
+#define SSID "mynet"
+#define PASSWORD "mypass"
+
+#define MQTT_BROKER "192.168.1.1"
+#define MQTT_TOPIC "topic"
+
+#define SOVEROM 0xab123456
+#define HUB 0xe0798765
+
+#endif
